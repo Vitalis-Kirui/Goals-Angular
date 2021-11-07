beb119be3ce379d41cccd087dc8599d4f2808608
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-11-07 22:55:13 +0300

    creating a service for api after hiding api url

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -4,6 +4,7 @@ import { GoalService } from '../goal-service/goal.service';
 import { AlertService } from '../alert-service/alert.service';
 import { Quote } from '../quote-class/quote';
 import { HttpClient } from '@angular/common/http';
+import { QuoteRequestService } from '../quote-http/quote-request.service';
 
 @Component({
   selector: 'app-goal',
@@ -19,7 +20,7 @@ export class GoalComponent implements OnInit {
 
   quote: Quote;
 
-  constructor(goalService: GoalService, alertService: AlertService, private http: HttpClient) {
+  constructor(goalService: GoalService, alertService: AlertService, private quoteService: QuoteRequestService) {
     this.goals = goalService.getGoals()
     this.alertService = alertService;
   }
@@ -52,17 +53,7 @@ export class GoalComponent implements OnInit {
 
   ngOnInit() {
 
-    interface ApiResponse {
-      author: string;
-      quote: string;
-    }
-
-    this.http.get<ApiResponse>("http://quotes.stormconsultancy.co.uk/random.json").subscribe(data => {
-      // Succesful API request
-      this.quote = new Quote(data.author, data.quote)
-    }, err => {
-      this.quote = new Quote("Winston Churchill", "Never never give up!")
-      console.log("An error occurred")
-    })
+    this.quoteService.quoteRequest()
+    this.quote = this.quoteService.quote
   }
 }
