2d199d551b0748d7c0eba18119cbdc1df12d58d5
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-11-07 14:38:02 +0300

    registering the service

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,31 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { Goal } from '../goal';
+import { GoalService } from '../goal-service/goal.service';
 
 @Component({
   selector: 'app-goal',
   templateUrl: './goal.component.html',
-  styleUrls: ['./goal.component.css']
+  styleUrls: ['./goal.component.css'],
+  providers: [GoalService]
 })
 export class GoalComponent implements OnInit {
-    addNewGoal(goal:any) {
-      let goalLength = this.goals.length;
-      goal.id = goalLength + 1;
-      goal.completeDate = new Date(goal.completeDate)
-      this.goals.push(goal)
-    }
-  toggleDetails(index) {
+
+  goals: Goal[];
+
+  constructor(goalService: GoalService) {
+    this.goals = goalService.getGoals()
+  }
+
+  addNewGoal(goal: any) {
+    let goalLength = this.goals.length;
+    goal.id = goalLength + 1;
+    goal.completeDate = new Date(goal.completeDate)
+    this.goals.push(goal)
+  }
+  toggleDetails(index:number) {
     this.goals[index].showDescription = !this.goals[index].showDescription;
-  } completeGoal(isComplete, index) {
+  } 
+
+  completeGoal(isComplete:boolean, index:number) {
     if (isComplete) {
       this.goals.splice(index, 1);
     }
   }
-  deleteGoal(isComplete, index) {
+  deleteGoal(isComplete:boolean, index:number) {
     if (isComplete) {
-      let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)      if (toDelete) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)     
+       if (toDelete) {
         this.goals.splice(index, 1)
       }
     }
   }
-  constructor() { } ngOnInit(): void {
+ngOnInit(): void {
   }
 }
