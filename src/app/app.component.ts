bbaf9a763aa7d74e4226dd55dee0f73624ffbdd2
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-11-07 08:40:29 +0300

    creating class Goal and its content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Goal } from './goal';
 
 @Component({
   selector: 'app-root',
@@ -6,10 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  goals : string[] ;
+  goals : Goal[] = [
 
-  constructor(){
-    this.goals = ['Watch Finding Nemo', 'Buy Cookies', 'Get new phone case']
-  }
+    { id: 1, name: 'Watch finding Nemo' },
+    { id: 2, name: 'Buy Cookies' },
+    { id: 3, name: 'Get new Phone Case' },
+    { id: 4, name: 'Get Dog Food' },
+    { id: 5, name: 'Solve math homework' },
+    { id: 6, name: 'Plot my world domination plan' },
+
+  ];
 
 }
